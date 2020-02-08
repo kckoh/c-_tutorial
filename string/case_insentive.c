908b3ead9d065752c782c6bd7fd48951ef2a0d81
kckoh <kyungcheolkoh@gmail.com>
2020-02-08 14:27:44 +0000

    trying to understand string

--- string/case_insentive.c
+++ string/case_insentive.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 
-int str_insensitive( const char* str0, const char* str1){
+int to_lower(const int str){
+    if(str>90){
+        return str -32;
+    }
+    return str;
 
-     while(*str0 != '\0' && *str1 == *str0){
-        ++str0;
-        ++str1;
+}
+
+int str_insensitive(const char* str0, const char* str1){
+    int c0 = to_lower(*str0);
+    int c1 = to_lower(*str1);
+     while(c0 != '\0' && c0 == c1){
+         str0++;
+         c0 = to_lower(*str0);
+         c1 = to_lower(*str1);
     }
 
-    return *str0 - *str1;
+    if(c1==c0){
+        return 0;
+    }
+
+    return c0>c1? 1:-1;
 
 }
 
 int main(void){
-    const char* str0 = "abcdefg";
+    const char* str0 = "abcdefg"; //stored in read only memory
     const char* str1 = "abcdefg";
+
     int compare_str = str_insensitive(str0,str1);
     printf("output is: %d\n", compare_str);
 }
