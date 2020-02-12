4988db848df93d1c3abbe3abe33b22bd85e966c8
kckoh <kyungcheolkoh@gmail.com>
2020-02-12 14:03:14 +0000

    learning file fgets and stuff

---
+++ file/read_char.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+# define LINE_LENGTH (8)
+int main(void){
+
+    //get a character and return the character
+    // int c = getchar();
+    // while(c != EOF){
+    //     putchar(c);
+    //     c = getchar();
+    // }
+
+    // reads a line and return the line
+    
+    char line[LINE_LENGTH];
+
+    while(fgets(line,LINE_LENGTH, stdin ) != NULL ){
+        //puts writes strings and return non-negative
+        printf("line: %s", line);
+    }
+
+    return 0;
+}
