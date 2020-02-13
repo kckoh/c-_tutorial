fb64c03cef83c9425ec15df98aa606ac1876ce7c
kckoh <kyungcheolkoh@gmail.com>
2020-02-13 13:28:54 +0000

    understanding file open with stream

---
+++ file/file_example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define LINE_LENGTH (1024)
+int main(void){
+    FILE* stream;
+    char list[LINE_LENGTH];
+
+    stream = fopen("hello.txt", 'r');
+    while( fgets(list, LINE_LENGTH , stream)  ){
+        printf("%s", list);
+
+    }
+
+
+
+    return 0;
+}
