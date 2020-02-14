60a1c013e9e304324f5da2911a86824f9ddec7c1
kckoh <kyungcheolkoh@gmail.com>
2020-02-14 14:53:12 +0000

    im confused with files

--- file/file_read.c
+++ file/file_read.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+#include <errno.h>
 #define LINE_LENGTH (1024)
+
 int main(void){
     FILE* stream;
     char list[LINE_LENGTH];
     //read
-    stream = fopen("hello.txt", "r");
-    while( fgets(list, LINE_LENGTH , stream)  ){
-        printf("%s", list);
+    stream = fopen("hello1.txt", "rb");
+    if(stream == NULL){
+      perror("error while opening");
     }
-
-
+    
+      if(fgets(list,LINE_LENGTH,stream) != NULL){
+        printf("%s", list);
+      }
+    
+    
+    if(fclose(stream) != 0 ){
+      fprintf(stderr, "Erro while clsoing");
+    }
+    fflush(stream);
 
     return 0;
 }
